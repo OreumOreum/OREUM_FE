dd1807f06589a11f72c88e76e371ea4e1b4efd58
Taylor2921 <kmth001123@gmail.com>
2025-07-14 11:05:51 +0900

    [feat] SearchScreen 구현

---
+++ lib/features/home/presentation/widgets/recent_search_list_tile.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:oreum_fe/core/constants/app_colors.dart';
+
+import 'package:oreum_fe/core/constants/app_sizes.dart';
+import 'package:oreum_fe/core/constants/icon_path.dart';
+import 'package:oreum_fe/core/themes/app_text_styles.dart';
+import 'package:oreum_fe/core/themes/text_theme_extension.dart';
+
+class RecentSearchListTile extends StatelessWidget {
+  final String title;
+
+  const RecentSearchListTile({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: AppSizes.iconSM,
+          width: AppSizes.iconSM,
+          child: Center(
+            child: SvgPicture.asset(
+              IconPath.clock,
+              width: 16.r,
+              height: 16.r,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 4.w,
+        ),
+        Expanded(
+          child: Text(
+            title,
+            style: context.textStyles.body1.copyWith(color: AppColors.gray200),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        SizedBox(
+          width: 6.w,
+        ),
+        SizedBox(
+          height: AppSizes.iconSM,
+          width: AppSizes.iconSM,
+          child: IconButton(
+            padding: EdgeInsets.zero,
+            constraints: const BoxConstraints(),
+            onPressed: () {  },
+            icon: SvgPicture.asset(
+              IconPath.searchCancel,
+              width: 10.r,
+              height: 10.r,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
