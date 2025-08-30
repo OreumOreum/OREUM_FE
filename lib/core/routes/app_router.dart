91d175d23d203275cc761a3ca591775ecc040bd5
kwakmincheol123 <arnold2246@gmail.com>
2025-08-30 19:55:18 +0900

    [bugfix] 단일여행지 뒤로갈때 리프레시 수정

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -409,7 +409,6 @@ GoRouter appRouter(AppRouterRef ref) {
           String? folderId = extraData['folderId'];
 
           return PlaceDetailScreen(
-            key: UniqueKey(), // 🔥 이 줄 추가 - 매번 새로운 위젯 인스턴스 생성
             placeId: placeId,
             contentId: contentId,
             contentTypeId: contentTypeId,
